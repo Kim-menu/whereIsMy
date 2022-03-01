cd62c0a8d6b1df4ad6ab9a716772ef9f70bb506a
Kim-menu <hyunwjin@gmail.com>
2022-03-01 13:47:04 +0900

    REFACTOR: make center column simple

--- lib/main.dart
+++ lib/main.dart
@@ -26,9 +26,7 @@ class MyHomePage extends StatelessWidget {
       body: Center(
         child: Column(
             children: [
-              const SizedBox(height: 50.0),
-              _image('assets/icon_home_page.png', 120.0),
-              const SizedBox(height: 120.0),
+              _imageBox(300.0, 'assets/icon_home_page.png', 170.0),
               _buildCard(),
             ],
           ),
@@ -52,19 +50,27 @@ class MyHomePage extends StatelessWidget {
     );
   }
 
-  Image _image(String file, double width) {
-    return Image(
-      image: AssetImage(file),
-      width: width,
+  Widget _imageBox(double boxHeight, String file, double imageWidth) {
+    return SizedBox(
+      height: boxHeight,
+      child: Center(
+        child: Image(
+          image: AssetImage(file),
+          width: imageWidth,
+        ),
+      ),
     );
   }
 
   Widget _tile(String title, Color? color) {
     return Expanded(
         child: ListTile(
-          title: Text(
-            title,
-            textAlign: TextAlign.center,
+          title: Center(
+            child:
+            Text(
+              title,
+              textAlign: TextAlign.center,
+            ),
           ),
           tileColor: color,
         )
